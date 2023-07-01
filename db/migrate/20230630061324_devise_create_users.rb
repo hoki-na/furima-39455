fffc134442869fd42a4ce40fb54c9657640a7269
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-01 15:22:48 +0900

    ユーザー登録、画面遷移

--- db/migrate/20230630061324_devise_create_users.rb
+++ db/migrate/20230630061324_devise_create_users.rb
@@ -4,15 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.date   :birthday,           null: false, 
+      t.string :first_name_kana,    null: false, 
+      t.string :last_name_kana,     null: false, 
+      t.string :first_name,         null: false, 
+      t.string :last_name,          null: false, 
+      t.string :nickname,           null: false, 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname, null:
-      t.string :first_name, null:
-      t.string :last_name, null:
-      t.string :first_name_kana, null:
-      t.string :last_name_kana, null:
-      t.date :birthday, null:
-
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
