a7050d540db857105579aab937a960763ee5506b
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-04 18:53:29 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { 'テスト' }
+    description { 'テストです' }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_charge_id { 2 }
+    prefecture_id { 2 }
+    ship_date_id { 2 }
+    price { 1000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
