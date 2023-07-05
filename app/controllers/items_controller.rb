159e6b4fe3040275842255f259be96d940b15d88
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-06 02:47:51 +0900

    商品一覧表示ルボコップ後

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,17 +8,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-  
 
     if @item.save
-      redirect_to root_path, notice: 'Item was successfully created.' 
+      redirect_to root_path, notice: 'Item was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   # def show
@@ -50,13 +49,11 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :delivery_charge_id,
+                                 :prefecture_id, :ship_date_id, :image).merge(user_id: current_user.id)
   end
 
   # def set_item
   #   @item = Item.find(params[:id])
   # end
-
-
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
@@ -10,10 +9,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :purchases
 
-
-
   validates :image, :item_name, :description, presence: true
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id, exclusion: { in: [1], message: "is not a valid option" }
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id,
+            exclusion: { in: [1], message: 'is not a valid option' }
   validates :price, presence: true,
                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 
@@ -25,4 +23,3 @@ class Item < ApplicationRecord
     purchases.exists?
   end
 end
-
