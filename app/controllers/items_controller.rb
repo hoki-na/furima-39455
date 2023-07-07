af6fe114f3dfd59c1ce7ac41764397323670702c
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-07 15:19:02 +0900

    商品詳細レビュー修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show]
 
 
   def new
@@ -22,10 +21,6 @@ class ItemsController < ApplicationController
     @items = Item.all.order('created_at DESC')
   end
 
-
-def show
-  @item = Item.find(params[:id])
-end
   # def edit
   #   return unless current_user.id != @item.user_id
 
