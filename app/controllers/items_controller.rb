c971717ca546282bf161ec8ce35c4203f943e021
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-14 14:29:09 +0900

    ページ遷移、クレジット機能前

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,23 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :show, :new, :edit, :destroy]
+  before_action :set_item, only: [:show]
+  before_action :user_check, only: [:show]
 
   def new
     @item = Item.new
   end
 
   def show
+    @user = @item.user
     
+    if user_signed_in? && user_check
+      @editable = true
+    end
   end
+  
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
     if @item.save
       redirect_to root_path, notice: 'Item was successfully created.'
     else
@@ -24,8 +30,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user != @item.user
-      redirect_to root_path
+    if current_user != @item.user || @item.sold_out?
+      redirect_to root_path and return
     end
   end
 
@@ -45,12 +51,16 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
+  private
+
   def set_item
     @item = Item.find(params[:id])
   end
 
-  private
+  def user_check
+    current_user == @item.user
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :delivery_charge_id,
---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,55 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
+
+  
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @purchase_address = PurchaseAddress.new
+    
+    if @item.sold_out?
+      redirect_to root_path
+    end
+    
+    if user_check
+      redirect_to root_path and return
+    end
+    
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      pay_item
+      
+      @purchase_address.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def purchase_address_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :house_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  private
+
+  def user_check
+    current_user == @item.user
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_address_params[:token],
+        currency: 'jpy'
+      )
+  end
+end
