0ad21f6a032e854f8472f55e26f1d5a186259041
hoki-na <135810704+hoki-na@users.noreply.github.com>
2023-07-20 00:05:14 +0900

    デプロイ後修正(ログアウト時詳細表示)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :user_check, only: [:show]
 
